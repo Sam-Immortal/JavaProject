9b8ac0bbf0cf23c29d1bbb4ff51b9fa213606431
unknown <shambhavi7.502@gmail.com>
2024-11-07 09:22:50 +0530

    test upload using vsc

--- tests/Test.java
+++ tests/Test.java
@@ -1,7 +1,12 @@
-class Test
+import java.io.File; //File class
+
+//import java.io.IOException; //IOException class to handle errors
+
+public class Test
 {
     public static void main (String[] args)
     {
-        System.out.println("HELLO WORLD JAVA");
+        File obj = new File("myflie.txt"); //File name
+        System.out.println("File Created");
     }
 }
