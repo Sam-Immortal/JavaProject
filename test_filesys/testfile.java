9c3636b82a2f266a67a29f1688bb6e380a646139
unknown <shambhavi7.502@gmail.com>
2024-11-11 13:27:15 +0530

    creating a file

---
+++ test_filesys/testfile.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+
+public class testfile{
+    public static void main(String[] args){
+        try {
+            File myObj = new File("File1.txt");
+            if(myObj.createNewFile()){
+                System.out.println("File created: " + myObj.getName());
+            }
+            else{
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }
+    }
+}
